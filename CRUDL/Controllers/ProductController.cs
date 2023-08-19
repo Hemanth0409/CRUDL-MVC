37cfd4305d79f9c160a930856556e959ea3479cd
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-08-19 10:21:02 +0530

    Add project files.

---
+++ CRUDL/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CRUDL.Models;
+using System.Web.Mvc;
+namespace CRUDL.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDetails productDetails;
+        // GET: Product
+        static IList<ProductDetails> products = new List<ProductDetails>
+        {
+            new ProductDetails(){ProId=01,ProName="Apple",Price=100,DeptName=dept.fruits}
+        };
+        public ActionResult DisplayTable()
+        {
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProId,ProName,Price,DeptName")] ProductDetails pro)
+        {
+            if (ModelState.IsValid)
+            {
+                products.Add(pro);
+                return RedirectToAction("DisplayTable");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var edit=products.Where(s=>s.ProId == Id).FirstOrDefault();
+            return View(edit);
+        }
+        [HttpPost]
+        public  ActionResult Edit(ProductDetails pro)
+        {
+            var edits = products.Where(s => s.ProId == pro.ProId).FirstOrDefault();
+            products.Remove(edits);
+            products.Add(pro);
+            return RedirectToAction("DisplayTable");
+        }
+        [HttpGet]
+        public ActionResult Details(int Id) {
+            var edit = products.Where(s => s.ProId == Id).FirstOrDefault();
+            return View(edit);
+        }
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            var delete = products.Where(s => s.ProId == Id).FirstOrDefault();
+            products.Remove(delete);
+            return RedirectToAction("DisplayTable");
+
+        }
+    }
+}
---
+++ CRUDL/Models/ProductDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUDL.Models
+{
+    public class ProductDetails
+    {
+        [Required]
+        public int ProId { get; set; }
+        [Required(ErrorMessage ="Enter Product Name")]
+        [StringLength(20,MinimumLength =3,ErrorMessage ="Name length must be more than 3")]
+        [RegularExpression(@"^([A-Za-z ]+)$")]
+        public string ProName { get; set; }
+        [Required(ErrorMessage = "Price")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Department")]
+        public dept DeptName { get; set; }
+
+    }
+    public enum dept
+    {
+        Snacks,
+        Drinks,
+        Vegetable,
+        fruits 
+    }
+}
